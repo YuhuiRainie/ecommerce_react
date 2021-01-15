36cf5b742c495a5924c0199c8ff0171a2e41a59f
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-15 16:48:57 +1100

    done data normalization with shop data

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './category.scss'
 import {selectCategory} from '../../redux/shop/shop-selector'
 import {connect} from 'react-redux'
+import CollectionItem from "../../components/collection-item/collection-item";
 
 const Category = ({category}) => {
-    console.log(category)
+    const {title,items} = category
     return (
-        <div className='category'>
-            <h2>CATEGORY PAGE</h2>
+        <div className='category-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {items.map (item =><CollectionItem key={item.id} item={item} />)}
+            </div>
         </div>
     );
 };
