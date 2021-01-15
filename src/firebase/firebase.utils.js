6cb7e1fe031ad41626a2aaa9821f4ea379cdb99d
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-15 22:48:05 +1100

    manage api keys

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,45 @@
+import firebase from "firebase/app";
+import 'firebase/auth'
+import 'firebase/firestore'
+import {config} from "./firebase-api-key";
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    console.log(snapShot)
+    console.log(userRef)
+    console.log(userAuth)
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
+
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
