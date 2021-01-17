56d02caedb3cd34eab19c6204bdbec7eee01b321
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-18 01:48:25 +1100

    change to hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import {store,persistor} from'./redux/store';
 import {PersistGate} from "redux-persist/integration/react";
@@ -10,11 +10,11 @@ import {PersistGate} from "redux-persist/integration/react";
 
 ReactDOM.render(
     <Provider store={store} >
-        <BrowserRouter>
+        <HashRouter>
             <PersistGate persistor={persistor}>
                 <App />
             </PersistGate>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
   ,
   document.getElementById('root')
