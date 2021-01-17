54db33361dfcf89551f7a8107d4d34c2166d1f83
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-18 09:43:36 +1100

    material ui

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,18 +1,19 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 import MenuItem from "../menu-item/menu-item";
-import './directory.scss'
+// import './directory.scss'
 import {connect} from 'react-redux'
 import {selectDirectorySections} from '../../redux/directory/directory-selector'
 import {createStructuredSelector} from "reselect";
+import Grid from '@material-ui/core/Container';
 
 const Directory =({sections}) => {
         return (
-            <div className='directory-menu'>
+            <Grid container className='directory-menu' direaction='row' spacing={2}>
                 {sections.map(({title, imageUrl, id,size, linkUrl}) => {
-                   return  <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
+                    return <Grid item> <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} /></Grid>
                 })}
-            </div>
+            </Grid>
         );
     }
 
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './homepage.scss'
+// import './homepage.scss'
 import Directory from "../../components/directory/directory";
 import MenuItem from "../../components/menu-item/menu-item";
-
+import Container from '@material-ui/core/Container';
 
 const Homepage = () => {
     return (
-        <div className='homepage'>
+        <Container className='homepage'>
             <Directory />
-        </div>
+        </Container>
     );
 };
 
