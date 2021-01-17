b3f7efb3a28a7eedec32532084bd35d10d9552c0
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-17 21:10:56 +1100

    deploy set up

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,10 @@ import rootReducer from './root-reducer';
 import {persistStore} from "redux-persist";
 
 
-const middlewares = [logger];
+const middlewares = [];
+if(process.env.NODE_ENV ==='development'){
+    middlewares.push(logger)
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store)
 export default  { store,persistor};
