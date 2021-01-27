7a297f634bacbfe48767c8526e3f36f3f8da91c5
YuhuiRainie <48782788+YuhuiRainie@users.noreply.github.com>
2021-01-28 02:50:20 +1100

    finish

---
+++ src/redux/shop/shop-saga.js
@@ -0,0 +1,6 @@
+import {takeEvery} from 'redux-saga/effects'
+import ShopActionTypes from "./shop-types";
+
+export function* fetchCollectionsStart() {
+
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from "redux-persist";
 import thunk from 'redux-thunk';
+import saga from 'redux-saga'
 
-
+// const sagaMiddleware = [sagaMiddleware]
 const middlewares = [thunk];
 if(process.env.NODE_ENV ==='development'){
     middlewares.push(logger)
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// sagaMiddleware.run
 export const persistor = persistStore(store)
 export default  { store,persistor};
