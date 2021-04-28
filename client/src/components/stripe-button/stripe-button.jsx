766067a056c4c01ee06d76d251dba7b8af77a0b5
Rainie Liu <rainie.liu@speedylinkage.com>
2021-04-28 10:23:14 +1000

    finish backend of strape payment button

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
 import {api} from "./stripe-api-key";
-
+import axios from "axios";
 const StripeButton = ({price}) => {
     const priceForStripe = price *100
     const publishKey = api
     const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful!');
+        axios({
+            url:'payment',
+            method:'post',
+            data:{
+                amount:priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert("payment successful")
+        }).catch(error => {
+            console.log('payment error:', JSON.parse(error))
+            alert('there was an issue with your payment. please change a card.')
+        })
     };
 
     return (
         <StripeCheckout
             label='Pay Now'
-            name='CRWN Clothing Ltd.'
+            name='Rainie Clothing Ltd.'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/CUz.svg'
