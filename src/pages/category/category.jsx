34b0e71805e0805aec79e05dc7f48de381029bb3
Rainie Liu <rainie.liu@speedylinkage.com>
2021-04-28 21:44:00 +1000

    replace redux by useContext api

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,34 +1,19 @@
-import React from 'react';
-// import './category.scss'
-import {selectCategory} from '../../redux/shop/shop-selector'
-import {connect} from 'react-redux'
+import React,{useContext} from 'react';
 import CollectionItem from "../../components/collection-item/collection-item";
 import {Grid} from "@material-ui/core";
 import CollectionsContext from "../../contexts/collections/collections.context";
 
 const Category = ({match}) => {
-    // const {title,items} = category;
+    const collections = useContext(CollectionsContext)
+    const collection = collections[match.params.categoryId]
+    const {title,items} = collection;
     return (
-        <CollectionsContext.Consumer>
-            {
-                collections => {
-                    const collection = collections[match.params.categoryId]
-                    const {title,items} = collection
-                    return(
-                        <div className='category-page'>
-                            <h1 className='title'>{title}</h1>
-                            <Grid container direction='row' spacing={2}>
-                                {items.map (item =><CollectionItem key={item.id} item={item} />)}
-                            </Grid>
-                        </div>
-                    )
-                }
-            }
-
-        </CollectionsContext.Consumer>
+        <div className='category-page'>
+            <h1 className='title'>{title}</h1>
+            <Grid container direction='row' spacing={2}>
+                {items.map (item =><CollectionItem key={item.id} item={item} />)}
+            </Grid>
+        </div>
     );
 };
-const mapStateToProps = (state,ownProps)=>({
-    category:selectCategory(ownProps.match.params.categoryId)(state)
-})
 export default Category;
